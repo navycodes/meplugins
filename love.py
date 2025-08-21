88ddd963e8cb98b036c61f6111097aa99159acd1
Ryn Lulzsec <152095647+Youghvee@users.noreply.github.com>
2025-08-21 09:41:39 +0700

    Update love.py

--- love.py
+++ love.py
@@ -1,8 +1,8 @@
+import config
 import random
+from core import app
 
 from pyrogram import filters
-
-from core import app
 
 
 __MODULE__ = "Love-Tools"
@@ -38,17 +38,15 @@ def get_random_message(love_percentage):
         )
 
 
-@app.on_message(filters.command("love", prefixes="/"))
-def love_command(client, message):
-    command, *args = message.text.split(" ")
-    if len(args) >= 2:
-        name1 = args[0].strip()
-        name2 = args[1].strip()
-
-        love_percentage = random.randint(10, 100)
-        love_message = get_random_message(love_percentage)
-
-        response = f"{name1}💕 + {name2}💕 = {love_percentage}%\n\n{love_message}"
-    else:
-        response = "Please enter two names after /love command."
-    app.send_message(message.chat.id, response)
+@app.on_message(filters.command("truth") & ~config.BANNED_USERS)
+async def love_cmd(client, message):
+    cmd, *args = message.text.split(" ")
+    user = message.from_user 
+    user_first_name = user.first_name
+    love_percentage = random.randint(10, 100)
+    love_message = get_random_message(love_percentage)
+    text = random.choice(love_percentage)
+    return await message.reply(
+        f"<blockquote expandable>{user} + {user} = {text}%\n\n{love_message}</blockquote>",
+        app.send_message(message.chat.id, text)
+    )
